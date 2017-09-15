263f8b4b57b133cb3b2b051706f28089c63966b2
pjsinco <psinco@gmail.com>
2017-09-15 17:31:08 -0500

    Create a users factory, seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -6,30 +6,61 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('users', function (Blueprint $table) {
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
-    }
+      $table->increments('id');
+      $table->string('name');
+      $table->string('password');
+      $table->rememberToken();
+      $table->timestamps();
+
+      // Personal
+      $table->string('first_name');
+      $table->string('middle_name')->nullable();
+      $table->string('last_name');
+      $table->string('suffix')->nullable();
+      $table->string('designation');
+      $table->string('email')->unique();
+      $table->string('website')->nullable();
+
+      // Contact - home
+      $table->string('home_addr_1');
+      $table->string('home_addr_2')->nullable();
+      $table->string('home_city');
+      $table->char('home_state', 2);
+      $table->string('home_zip');
+      $table->string('home_phone');
+
+      // Contact - home
+      $table->string('bus_addr_1')->nullable();
+      $table->string('bus_addr_2')->nullable();
+      $table->string('bus_city')->nullable();
+      $table->char('bus_state', 2)->nullable();
+      $table->string('bus_zip')->nullable();
+      $table->string('bus_phone')->nullable();
+
+      // Membership
+      $table->integer('aoa_id')->unsigned();
+      $table->char('status', 4);
+      $table->datetime('paid_thru')->nullable();
+      $table->string('member_type');
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('users');
+  }
 }
