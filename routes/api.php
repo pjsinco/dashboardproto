58e3bce55b7b35fb4c0d23d4b6d4d8464b203da0
pjsinco <psinco@gmail.com>
2017-09-16 16:31:26 -0500

    Add API route: /api/v1/doctor/{id}

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+    //return $request->user();
+//});
+
+Route::get('/v1/doctor/{id}', function($id) {
+  return App\User::findOrFail($id);
 });
