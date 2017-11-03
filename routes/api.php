f837a04afd47689828aeef0d0d23ffdfd709795b
pjsinco <psinco@gmail.com>
2017-11-03 10:43:55 -0500

    Housekeeping

--- routes/api.php
+++ routes/api.php
@@ -17,45 +17,12 @@ use Illuminate\Http\Request;
     //return $request->user();
 //});
 
+//Route::get('/v1/users/{user}', 'UsersController@show')->middleware('auth:api');
 Route::get('/v1/users/{user}', 'UsersController@show');
 
 Route::post('/v1/users/{user}', 'UsersController@update');
 
-// TODO fix
-// For now, return the same json every time
 Route::get('/v1/users/{id}/cme', function($id) {
-
-//  $data = [
-//    "aoa_id" =>  "269802",
-//    "general" =>  [
-//      "earned" =>  67.0,
-//      "cat1aEarned" =>  26.0
-//    ],
-//    "primaries" =>  [
-//      [
-//        "desc" =>  "Radiology",
-//        "earned" =>  25.5,
-//        "required" =>  50.0,
-//        "generalRequired" =>  96.0,
-//        "cat1aRequired" =>  0.0,
-//        "subs" =>  [
-//          [
-//            "desc" =>  "Radiation Therapy",
-//            "earned" =>  1.5,
-//            "required" =>  13.0
-//          ]
-//        ]
-//      ],
-//      [
-//        "desc" =>  "Family Medicine",
-//        "earned" =>  23.0,
-//        "required" =>  30.0,
-//        "generalRequired" =>  150.0,
-//        "cat1aRequired" =>  30.0,
-//        "subs" =>  []
-//      ]
-//    ]
-//  ];
 
   $data = [
     [
