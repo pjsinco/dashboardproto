e9f4c507eb141e3b2bf8ad7db0c89a9c3ddf236f
pjsinco <psinco@gmail.com>
2017-11-03 11:47:58 -0500

    Create Address model and Factory method

---
+++ database/factories/AddressFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Address::class, function (Faker $faker) {
+  return [
+    'aoa_id' => function() {
+      $user = factory('App\User')->create();
+      return $user->aoa_id;
+    },
+
+    'home_addr_1' => sprintf('%s %s', 
+                             $faker->buildingNumber,
+                             $faker->streetName),
+    'home_addr_2' => $faker->optional()->secondaryAddress,
+    'home_city' => $faker->city,
+    'home_state' => $faker->stateAbbr,
+    'home_zip' => $faker->postcode,
+    'home_phone' => $faker->phoneNumber,
+
+    'bus_addr_1' => sprintf('%s %s', 
+                             $faker->buildingNumber,
+                             $faker->streetName),
+    'bus_addr_2' => $faker->optional()->secondaryAddress,
+    'bus_city' => $faker->city,
+    'bus_state' => $faker->stateAbbr,
+    'bus_zip' => $faker->postcode,
+    'bus_phone' => $faker->phoneNumber,
+  
+  ];
+});
---
+++ database/migrations/2017_11_03_162705_create_addresses_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressesTable extends Migration
+{
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::create('addresses', function (Blueprint $table) {
+
+      $table->integer('aoa_id')->unsigned();
+      $table->timestamps();
+
+      // Contact - home
+      $table->string('home_addr_1');
+      $table->string('home_addr_2')->nullable();
+      $table->string('home_city');
+      $table->char('home_state', 2);
+      $table->string('home_zip');
+      $table->string('home_phone');
+
+      // Contact - home
+      $table->string('bus_addr_1')->nullable();
+      $table->string('bus_addr_2')->nullable();
+      $table->string('bus_city')->nullable();
+      $table->char('bus_state', 2)->nullable();
+      $table->string('bus_zip')->nullable();
+      $table->string('bus_phone')->nullable();
+
+    });
+  }
+
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    Schema::dropIfExists('addresses');
+  }
+}
